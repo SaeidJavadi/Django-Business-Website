a41681e62c4bfa0d4a994cf6b62e31e454dc09c5
Saeid Javadi <SjavadiP@gmail.com>
2021-02-17 20:51:12 +0330

    remove old functions

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,40 +7,6 @@ from pytz import timezone
 from jdatetime import datetime as dt
 
 
-# class RegisterUserForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ('email', 'full_name', 'dateofbirth', 'phone', 'password')
-#
-#         widgets = {
-#             # 'email':forms.EmailField(),
-#             'phone': forms.NumberInput(),
-#             'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', }),
-#         }
-#         # labels = {
-#         #     'password':'رمز عبور',
-#         # }
-#
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data["password"])
-#         if commit:
-#             user.save()
-#         return user
-
-
-# class RegisterFormAdmin(RegisterUserForm):
-#     class Meta:
-#         widgets = {
-#             # 'email':forms.EmailField(),
-#             'phone': forms.NumberInput(
-#                 attrs={'class': 'form-control', 'placeholder': '09 - - - - - - - - -', 'type': 'tel', 'maxlength': '11',
-#                        'minlength': '11'}),
-#             'full_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'نام و نام خانواگی', }),
-#             'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', }),
-#         }
-
-
 class UserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
     password2 = forms.CharField(label=_('Password confirmation'), widget=forms.PasswordInput)
