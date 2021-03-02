f680e9c01ac3845fc7842279baba9cdfbd04d484
Saeid Javadi <SjavadiP@gmail.com>
2021-03-02 20:26:54 +0330

    fix prblm login register page for login user

--- accounts/views.py
+++ accounts/views.py
@@ -8,55 +8,61 @@ from accounts.tasks import checkbirth
 
 
 def userLogin(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            if User.objects.filter(email=cd['email']).exists():
-                user = authenticate(request, username=cd['email'], password=cd['password'])
-                if user is not None:
-                    login(request, user)
-                    messages.success(request, _('logged in successfully'), 'success')
-                    return redirect('base:index')
+    if not request.user.is_active:
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                if User.objects.filter(email=cd['email']).exists():
+                    user = authenticate(request, username=cd['email'], password=cd['password'])
+                    if user is not None:
+                        login(request, user)
+                        messages.success(request, _('logged in successfully'), 'success')
+                        return redirect('base:index')
+                    else:
+                        messages.error(request, _('your Email Or Password is wrong'), 'warning')
                 else:
-                    messages.error(request, _('your Email Or Password is wrong'), 'warning')
+                    messages.error(request, _('No account created with this email'),
+                                   'warning')
+                    return redirect('accounts:login')
             else:
-                messages.error(request, _('No account created with this email'),
-                               'warning')
-                return redirect('accounts:login')
+                messages.error(request, _('Please enter your information correctly'), 'warning')
         else:
-            messages.error(request, _('Please enter your information correctly'), 'warning')
+            form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
     else:
-        form = LoginForm()
-    return render(request, 'accounts/login.html', {'form': form})
+        return redirect('base:index')
 
 
 def userRegister(request):
-    form = RegisterForm()
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            birth = cd['dateofbirth']
-            if not User.objects.filter(email=cd['email']).exists():
-                if checkbirth(birth):
-                    if not User.objects.filter(phone=cd['phone']).exists():
-                        if not User.objects.filter(idcode=cd['idcode']).exists():
-                            user = User.objects.create_user(email=cd['email'], full_name=cd['full_name'],
-                                                            dateofbirth=cd['dateofbirth'],
-                                                            phone=cd['phone'], idcode=cd['idcode'], password=cd['password1'])
-                            user.save()
-                            messages.success(request, _('you registered successfully'), 'success')
-                            return redirect('accounts:login')
+    if not request.user.is_active:
+        form = RegisterForm()
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                birth = cd['dateofbirth']
+                if not User.objects.filter(email=cd['email']).exists():
+                    if checkbirth(birth):
+                        if not User.objects.filter(phone=cd['phone']).exists():
+                            if not User.objects.filter(idcode=cd['idcode']).exists():
+                                user = User.objects.create_user(email=cd['email'], full_name=cd['full_name'],
+                                                                dateofbirth=cd['dateofbirth'],
+                                                                phone=cd['phone'], idcode=cd['idcode'], password=cd['password1'])
+                                user.save()
+                                messages.success(request, _('you registered successfully'), 'success')
+                                return redirect('accounts:login')
+                            else:
+                                messages.success(request,_('this idcode is exists'))
                         else:
-                            messages.success(request,_('this idcode is exists'))
+                            messages.success(request,_('this phone is exists'))
                     else:
-                        messages.success(request,_('this phone is exists'))
+                        messages.success(request, _('People under the age of 18 are not allowed to register'), 'warning')
                 else:
-                    messages.success(request, _('People under the age of 18 are not allowed to register'), 'warning')
-            else:
-                messages.success(request, _('this Email is exists'),'warning')
-    return render(request, 'accounts/register.html', {'form': form})
+                    messages.success(request, _('this Email is exists'),'warning')
+        return render(request, 'accounts/register.html', {'form': form})
+    else:
+        return redirect('base:index')
 
 
 def LogoutPage(request):
