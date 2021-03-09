e0d9b41ad0e4439d84aab4c1fb94c1ebc5874ce9
Saeid Javadi <SjavadiP@gmail.com>
2021-03-09 20:35:38 +0330

    add idcode checker function

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -11,3 +11,24 @@ def checkbirth(date):
         return True
     else:
         return False
+
+
+def checkidcode(value):
+    a = int(value[-1])
+    print(a)
+    i = 0
+    b = 0
+    for num in reversed(range(2, 11)):
+        b += int(value[i]) * num
+        print(f'{value[i]}*{num}')
+        i += 1
+    c = b - (b / 11) * 11
+    print(b, c)
+    if c == 0 and a == c:
+        return True
+    elif c == 1 and a == 1:
+        return True
+    elif c > 1 and (a == c - 11):
+        return True
+    else:
+        return False
